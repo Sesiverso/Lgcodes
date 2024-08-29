dc2e556e19e124b4140d48f2f6c615ab0230f620
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-08-29 09:36:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-emailjs.init("7mn4fzN7hjmzs5knD"); // Substitua pela sua chave pública
-
-document.getElementById('orcamento-form').addEventListener('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    const nome = document.getElementById('nome').value;
-    const email = document.getElementById('email').value;
-    const servico = document.getElementById('servico').value;
-    const descricao = document.getElementById('descricao').value;
 
-    emailjs.send("service_odoytvp", "template_fg9ijtm", {
-        from_name: nome,
-        from_email: email,
-        service: servico,
-        description: descricao
-    })
-    .then(function(response) {
-        alert('Orçamento enviado com sucesso!');
-    }, function(error) {
-        console.error('Erro ao enviar o orçamento:', error);
-        alert('Ocorreu um erro ao enviar o orçamento.');
-    });
+    emailjs.sendForm('service_odoytvp', 'template_fg9ijtm', this)
+        .then(function() {
+            alert('Mensagem enviada com sucesso!');
+        }, function(error) {
+            alert('Ocorreu um erro ao enviar sua mensagem, tente novamente.');
+        });
 });
