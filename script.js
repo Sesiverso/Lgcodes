92319a8ed2dc08c840a827c07f61e03372045538
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-08-29 09:57:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+// Inicialize o EmailJS com sua chave pública
+(function() {
+    emailjs.init("7mn4fzN7hjmzs5knD");
+})();
+
+// Função para enviar o formulário usando EmailJS
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    emailjs.sendForm('service_odoytvp', 'template_fg9ijtm', this)
+    const serviceID = 'service_odoytvp';
+    const templateID = 'template_fg9ijtm';
+
+    emailjs.sendForm(serviceID, templateID, this)
         .then(function() {
             alert('Sua mensagem foi enviada com sucesso! Entraremos em contato em breve.');
         }, function(error) {
+            console.log('Erro:', error);
             alert('Ocorreu um erro ao enviar sua mensagem. Por favor, tente novamente.');
         });
 });
